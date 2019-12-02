d67b8c6d55519232a2df16caefd7a401115602a9
Alaedine Benani D <43844475+alaedinebe@users.noreply.github.com>
2019-12-02 18:02:55 +0100

    some changes
    sorry for not documenting

--- main.py
+++ main.py
@@ -4,38 +4,46 @@ from PIL import Image #to crop
 from fpdf import FPDF #to create the PDF
 from tqdm import tqdm_notebook #to get some progress insights
 
-print("Hi there! How many pages do you want to screenshot?")
-n = input()
+print("What do you want to do?\nEnter 1 for screenshot\nEnter 2 for cropping")
 
-print("Ok! We'll start as soon as you'll press 'S'")
+choice = input()
 
-keyboard.wait('S')
+if choice == 1:
 
-for i in range(1,int(n),1) :
-    keyboard.press_and_release('print_screen')
-    time.sleep(3) # to be sure that we're in the next page
-    keyboard.press_and_release('right arrow')
-    time.sleep(3) # to be sure that we're in the next page
+    print("Hi there! How many pages do you want to screenshot?")
+    n = input()
 
-# You might want to crop the image
+    print("Ok! We'll start as soon as you'll press 'S'")
 
-links = "C:\\Users\\BarackObama\\BidenAndIPictures\\img" # You need to customize that bro
+    keyboard.wait('S')
 
-for i in range(0,n,1):
-    links = "C:\\Users\\User\\OneDrive\\3 Old\\Autres\\Images\\Captures d’écran\\2019-11-28 (" + str(i) + ").png"
-    images = Image.open(links)
-    cropped_images = images.crop((530,0,1722,1504)) #left, top, right, bottom
-    cropped_images.save(str(i)+".png")
+    for i in range(0,int(n),1) :
+        keyboard.press_and_release('print_screen')
+        time.sleep(2) # to be sure that we're in the next page
+        keyboard.press_and_release('right arrow')
+        time.sleep(2) # to be sure that we're in the next page
+        
+elif choice == 2 :
     
-# You might want to convert the png to jpeg
+    print("Hi there! How many pages do you want to crop?")
+    n = input()
 
+    # You might want to crop the image
+    
+    print("x1")
+    x1 = input()
+    print("y1")
+    y1 = input()
+    print("x2")
+    x2 = input()
+    print("y2")
+    y2 = input()
 
-# You might want to create a pdf from the list of png
-
-pdf = FPDF()
-for i in tqdm_notebook(range(0,n,1)):
-    pdf.add_page()
-    pdf.image(str(i)+".png",0,0,210)
-pdf.output("EM-endoc15.pdf", "F")
+    for i in range(0,int(n),1):
+        links = str(i) + ".png"
+        images = Image.open(links)
+        cropped_images = images.crop((x1,y1,x2,y2)) #left, top, right, bottom
+        cropped_images.save(str(i)+".png")
+    
 
 print("All done duddy :)")
