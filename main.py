da507130d4212fb4cb7aa9a973feed62e8c3ce24
Alaedine Benani D <43844475+alaedinebe@users.noreply.github.com>
2019-12-02 17:40:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
-# TODO vefore executing this code : 1. open the file containing the subject of interest 2. put in full screen (if hard/impossible, open it with chrome and press F11) 3. Press the key S 4. It will automatically do a screenshot (should be saved in OneDrive) and move to the next page
-# PS : you might want to customize this code a little bit (mostly if it doesn't go to the next page with ->)
+import keyboard #to screenshot
+import time #to pause
+from PIL import Image #to crop
+from fpdf import FPDF #to create the PDF
+from tqdm import tqdm_notebook #to get some progress insights
 
-import keyboard
-import time
-
-print("how many pages do you want to screenshot?")
-
+print("Hi there! How many pages do you want to screenshot?")
 n = input()
 
 print("Ok! We'll start as soon as you'll press 'S'")
 
 keyboard.wait('S')
 
-for i in range(1,n,1):
-  keyboard.press_and_release('print_screen')
-  keyboard.press_and_release('right arrow')
-  time.sleep(5) # to be sure that we're in the next page
-  
-print("We're done here :)")
+for i in range(1,int(n),1) :
+    keyboard.press_and_release('print_screen')
+    time.sleep(3) # to be sure that we're in the next page
+    keyboard.press_and_release('right arrow')
+    time.sleep(3) # to be sure that we're in the next page
+
+# You might want to crop the image
+
+links = "C:\\Users\\BarackObama\\BidenAndIPictures\\img" # You need to customize that bro
+
+for i in range(0,n,1):
+    links = "C:\\Users\\User\\OneDrive\\3 Old\\Autres\\Images\\Captures d’écran\\2019-11-28 (" + str(i) + ").png"
+    images = Image.open(links)
+    cropped_images = images.crop((530,0,1722,1504)) #left, top, right, bottom
+    cropped_images.save(str(i)+".png")
+    
+# You might want to convert the png to jpeg
+
+
+# You might want to create a pdf from the list of png
+
+pdf = FPDF()
+for i in tqdm_notebook(range(0,n,1)):
+    pdf.add_page()
+    pdf.image(str(i)+".png",0,0,210)
+pdf.output("EM-endoc15.pdf", "F")
+
+print("All done duddy :)")
